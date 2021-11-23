0cefcb26f0435fc4aeedd29442f023a4f6cb2fae
George - David Apostolidis <iis20001@uom.edu.gr>
2021-11-23 23:00:42 +0200

    FIrst Commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-test
+const express = require("express");
+const app = express();
+
+app.get("/", (res,req) => {
+    req.send("Hello World");
+});
+
+app.listen(3000, () => {
+    console.log("Listening to 3000");
+});
